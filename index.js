1b3b8a75753d7b309a24ceea45ab80b80961565c
Fernando <gutierrezt.lfernando@gmail.com>
2024-05-22 16:49:58 -0500

    consultas para proyectos

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const mysql = require("mysql2");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const multer = require("multer");
-require('dotenv').config();
+require("dotenv").config();
 // Ahora puedes acceder a tus variables de entorno
 const app = express();
 const port = process.env.PORT;
@@ -37,7 +37,6 @@ db.connect((err) => {
   }
 });
 
-
 app.get("/listUsuarios", (req, res) => {
   db.query("SELECT User_Name FROM T_Usuario;", (err, result) => {
     if (err) {
@@ -52,18 +51,11 @@ app.get("/listUsuarios", (req, res) => {
 //Agregar una nuevo ususario
 app.post("/newUser", (req, res) => {
   const nuevoUsuario = req.body;
-  const {
-    Gmail,
-    Nombres,
-    Apellidos,
-    Contacto,
-    User,
-    Password,
-  } = nuevoUsuario;
+  const { Gmail, Nombres, Apellidos, Contacto, User, Password } = nuevoUsuario;
 
   encriptarContrasena(Password)
     .then((hash) => {
-      const consulta = `INSERT INTO Empleado (Gmail, Nombres, Apellidos, Informacion_Contacto, Usuario, Contrasena)
+      const consulta = `INSERT INTO T_Usuario (Gmail, Nombres, Apellidos, Informacion_Contacto, User_Name, User_Pass)
   VALUES ('${Gmail}', '${Nombres}', '${Apellidos}', '${Contacto}', '${User}', '${hash}');`;
       db.query(consulta, (err, result) => {
         if (err) {
@@ -90,12 +82,14 @@ app.post("/modUser", (req, res) => {
     User,
     Password,
     idUser,
+    Cod_Image_Perfil,
   } = nuevosDatos;
   const setValues = {
     Gmail: Gmail,
     Nombres: Nombres,
     Apellidos: Apellidos,
     Informacion_Contacto: Contacto,
+    Cod_Image_Perfil: Cod_Image_Perfil,
   };
   const consulta = "UPDATE T_Usuario SET ? WHERE ID_User = ?";
   db.query(consulta, [setValues, idUser], (err, result) => {
@@ -162,7 +156,7 @@ app.post("/newFile", upload.single("file"), (req, res) => {
   const { nombre } = req.body;
   const fileBuffer = req.file.buffer;
 
-  // Aquí deberías insertar el archivo y otros datos en tu base de datos
+  // Insertar el archivo y otros datos en la base de datos
   const sql = "INSERT INTO T_Archivos (Nombre, Archivo) VALUES (?, ?)";
   db.query(sql, [nombre, fileBuffer], (error, results, fields) => {
     if (error) {
@@ -172,7 +166,14 @@ app.post("/newFile", upload.single("file"), (req, res) => {
         .json({ error: "Error al insertar en la base de datos" });
     }
 
-    res.json({ success: true, message: "Archivo subido correctamente" });
+    // Obtener el ID_File del archivo insertado
+    const idFile = results.insertId;
+
+    res.json({
+      success: true,
+      idFile,
+      message: "Archivo subido correctamente",
+    });
   });
 });
 
@@ -180,7 +181,10 @@ app.get("/getIdNamesFile", (req, res) => {
   const sql = "SELECT ID_File, Nombre FROM T_Archivos";
   db.query(sql, (error, results, fields) => {
     if (error) {
-      console.error("Error al obtener IDs y nombres de la base de datos:", error);
+      console.error(
+        "Error al obtener IDs y nombres de la base de datos:",
+        error
+      );
       return res
         .status(500)
         .json({ error: "Error al obtener IDs y nombres de la base de datos" });
@@ -211,3 +215,149 @@ app.post("/getFileById", (req, res) => {
   });
 });
 
+//-------CONTROL DE PROYECTOS---------------
+app.post("/addProject", (req, res) => {
+  const { idAdministrador, descripcion, fechaInicial, fechaFinal, titulo } =
+    req.body;
+
+  // Validar los datos recibidos
+  if (
+    !idAdministrador ||
+    !fechaInicial ||
+    !fechaFinal ||
+    !titulo
+  ) {
+    return res.status(400).json({ error: "Todos los campos son obligatorios" });
+  }
+  // Convertir las fechas al formato 'YYYY-MM-DD'
+  const formattedFechaInicial = new Date(fechaInicial).toISOString().split('T')[0];
+  const formattedFechaFinal = new Date(fechaFinal).toISOString().split('T')[0];
+
+  const sqlInsertProject = `
+    INSERT INTO T_Proyecto (ID_Administrador, Descripcion, Fecha_Inicial, Fecha_Final,Titulo)
+    VALUES (?, ?, ?, ?,?)
+  `;
+
+  db.query(
+    sqlInsertProject,
+    [idAdministrador, descripcion, formattedFechaInicial, formattedFechaFinal, titulo],
+    (error, results) => {
+      if (error) {
+        console.error(
+          "Error al agregar el proyecto a la base de datos:",
+          error
+        );
+        return res
+          .status(500)
+          .json({ error: "Error al agregar el proyecto a la base de datos" });
+      }
+
+      const projectId = results.insertId;
+
+      // Asociar el administrador con el proyecto en la tabla T_Proyecto_User
+      const sqlAssociateAdmin = `
+      INSERT INTO T_Proyecto_User (ID_proyecto, ID_user)
+      VALUES (?, ?)
+    `;
+
+      db.query(sqlAssociateAdmin, [projectId, idAdministrador], (error) => {
+        if (error) {
+          console.error(
+            "Error al asociar el administrador con el proyecto:",
+            error
+          );
+          return res
+            .status(500)
+            .json({
+              error: "Error al asociar el administrador con el proyecto",
+            });
+        }
+
+        res
+          .status(201)
+          .json({ message: "Proyecto agregado con éxito", projectId });
+      });
+    }
+  );
+});
+
+app.post("/getProjectsByUserId", (req, res) => {
+  const { idUsuario } = req.body;
+
+  if (!idUsuario) {
+    return res.status(400).json({ error: "ID de usuario es obligatorio" });
+  }
+
+  const sqlGetProjects = `
+    SELECT p.ID_Proyecto, p.Descripcion, p.Fecha_Inicial, p.Fecha_Final, p.Titulo 
+    FROM T_Proyecto p
+    INNER JOIN T_Proyecto_User pu ON p.ID_Proyecto = pu.ID_proyecto
+    WHERE pu.ID_user = ?
+  `;
+
+  db.query(sqlGetProjects, [idUsuario], (error, results) => {
+    if (error) {
+      console.error(
+        "Error al obtener los proyectos de la base de datos:",
+        error
+      );
+      return res
+        .status(500)
+        .json({ error: "Error al obtener los proyectos de la base de datos" });
+    }
+
+    res.json({ proyecto: results });
+  });
+});
+
+app.put("/updateProject", (req, res) => {
+  const { idProyecto, idAdministrador, descripcion, fechaInicial, fechaFinal,titulo } =
+    req.body;
+
+  // Validar los datos recibidos
+  if (
+    !idProyecto ||
+    !idAdministrador ||
+    !descripcion ||
+    !fechaInicial ||
+    !fechaFinal ||
+    !titulo
+  ) {
+    return res.status(400).json({ error: "Todos los campos son obligatorios" });
+  }
+
+  const sqlUpdateProject = `
+    UPDATE T_Proyecto 
+    SET ID_Administrador = ?, Descripcion = ?, Fecha_Inicial = ?, Fecha_Final = ?, Titulo = ?
+    WHERE ID_Proyecto = ?
+  `;
+
+  db.query(
+    sqlUpdateProject,
+    [idAdministrador, descripcion, fechaInicial, fechaFinal, titulo, idProyecto],
+    (error, results) => {
+      if (error) {
+        console.error(
+          "Error al actualizar el proyecto en la base de datos:",
+          error
+        );
+        return res
+          .status(500)
+          .json({
+            error: "Error al actualizar el proyecto en la base de datos",
+          });
+      }
+
+      if (results.affectedRows === 0) {
+        return res.status(404).json({ error: "Proyecto no encontrado" });
+      }
+
+      res.status(200).json({ message: "Proyecto actualizado con éxito" });
+    }
+  );
+});
+
+//-----------------------------SUBIR AL SERvIDOR-------------------------//
+app.listen(port, () => {
+  console.log(`Servidor backend en ejecución en http://localhost:${port}`);
+});
